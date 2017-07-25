1ce1c94c3b1fe72a7617e425d8867fd86791ced0
magichill33 <986417576@qq.com>
2017-07-25 15:29:00 +0800

    修改了相关布局

--- app/src/main/java/com/nfu/old/activity/MainActivity.java
+++ app/src/main/java/com/nfu/old/activity/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 clearSelected();
                 btnHome.setNfuSeleted(true);
+                setHomeFragment();
             }
         });
 
