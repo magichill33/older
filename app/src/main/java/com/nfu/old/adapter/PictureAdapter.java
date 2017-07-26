41c35432a6e2f3e2996a948f1036379eebd6e57f
ly666 <424505557@qq.com>
2017-07-26 11:30:49 +0800

    修改服务相关代码逻辑

--- app/src/main/java/com/nfu/old/adapter/PictureAdapter.java
+++ app/src/main/java/com/nfu/old/adapter/PictureAdapter.java
@@ -1,7 +1,10 @@
 package com.nfu.old.adapter;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -14,20 +17,23 @@ import com.nfu.old.fragment.ServiceFragment;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.nfu.old.R.attr.iconDrawablePress;
+
 /**
  * Created by Administrator on 2017/7/25.
  */
 
 public class PictureAdapter extends BaseAdapter {
     private Context context;
-
+    Integer[] mImagesPress;
+    Integer[] mImages;
     private List<Picture> pictures=new ArrayList<Picture>();
 
-    public PictureAdapter(String[] titles, Integer[] images, Context context) {
+    public PictureAdapter(String[] titles, Integer[] images, Integer[] imagesPress ,Context context) {
         super();
         this.context = context;
-
-
+        this.mImagesPress = imagesPress;
+        this.mImages = images;
         for (int i = 0; i < images.length; i++) {
             Picture picture = new Picture(titles[i], images[i]);
             pictures.add(picture);
@@ -56,7 +62,9 @@ public class PictureAdapter extends BaseAdapter {
 
         return position;
     }
-
+    ImageView itemBtn;
+    TextView itemTv;
+    View view;
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
@@ -76,16 +84,122 @@ public class PictureAdapter extends BaseAdapter {
         } else {
             // 取得converHolder附加的对象
             viewHolder = (ViewHolder) convertView.getTag();
+
         }
 
         // 给组件设置资源
         Picture picture = pictures.get(position);
         viewHolder.image.setImageResource(picture.getImageId());
         viewHolder.title.setText(picture.getTitle());
+        viewHolder.image.setTag(position);
 
+        convertView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                view  =v;
+
+                itemBtn = (ImageView) v.findViewById(R.id.item_pic);
+                itemTv = (TextView) v.findViewById(R.id.item_pic_title);
+                int  position = (int)itemBtn.getTag();
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    Log.d("PictureAdapter","ACTION_DOWN  position = " +position );
+                    switch (position) {
+                        case 0:
+                            changeDown(0);
+
+                            break;
+                        case 1:
+                            changeDown(1);
+                            break;
+                        case 2:
+                            changeDown(2);
+                            break;
+                        case 3:
+                            changeDown(3);
+                            break;
+                        case 4:
+                            changeDown(4);
+                            break;
+                        case 5:
+                            changeDown(5);
+                            break;
+                        case 6:
+                            changeDown(6);
+                            break;
+                        case 7:
+                            changeDown(7);
+                            break;
+                        case 8:
+                            changeDown(8);
+                            break;
+                        case 9:
+                            changeDown(9);
+                            break;
+                    }
+                    return true;
+                }else if(event.getAction() == MotionEvent.ACTION_MOVE){
+                    Log.d("PictureAdapter","ACTION_MOVE position = " +position );
+                    return false;
+                } else if(event.getAction() == MotionEvent.ACTION_UP||event.getAction() == MotionEvent.ACTION_CANCEL){
+                    if(event.getAction() == MotionEvent.ACTION_UP){
+                        Log.d("PictureAdapter","ACTION_UP  position = " +position);
+                    }else if(event.getAction() == MotionEvent.ACTION_CANCEL) {
+                        Log.d("PictureAdapter","ACTION_CANCEL position = " +position );
+                    }
+                    switch (position) {
+                        case 0:
+                            changeUp(0);
+                            break;
+                        case 1:
+                            changeUp(1);
+                            break;
+                        case 2:
+                            changeUp(2);
+
+                            break;
+                        case 3:
+                            changeUp(3);
+                            break;
+                        case 4:
+                            changeUp(4);
+                            break;
+                        case 5:
+                            changeUp(5);
+                            break;
+                        case 6:
+                            changeUp(6);
+                            break;
+                        case 7:
+                            changeUp(7);
+                            break;
+                        case 8:
+                            changeUp(8);
+                            break;
+                        case 9:
+                            changeUp(9);
+                            break;
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
         return convertView;
     }
-
+    private void changeDown(int position) {
+        //textView颜色变换，item背景切换，imageview切换
+//        itemBtn.setBackgroundColor(mImagesPress[position]);
+        itemBtn.setImageDrawable(context.getResources().getDrawable(mImagesPress[position]));
+        view.setBackgroundColor(context.getResources().getColor(R.color.base_red_color));
+        itemTv.setTextColor(Color.WHITE);
+    }
+    private void changeUp(int position) {
+        //textView颜色变换，item背景切换，imageview切换
+//        itemBtn.setBackgroundColor(mImages[position]);
+        itemBtn.setImageDrawable(context.getResources().getDrawable(mImages[position]));
+        view.setBackgroundColor(context.getResources().getColor(R.color.white));
+        itemTv.setTextColor(context.getResources().getColor(R.color.base_red_color));
+    }
     class ViewHolder {
         public TextView title;
         public ImageView image;
