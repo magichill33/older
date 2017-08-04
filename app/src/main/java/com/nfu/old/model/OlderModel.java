bcfb9742ee6002d9498201e3e94a1e93c9da3f2a
magichill33 <986417576@qq.com>
2017-08-04 16:44:44 +0800

    添加了养老资讯相关代码

---
+++ app/src/main/java/com/nfu/old/model/OlderModel.java
@@ -0,0 +1,42 @@
+package com.nfu.old.model;
+
+/**
+ * Created by Administrator on 2017-8-4.
+ */
+
+public class OlderModel {
+
+    /**
+     * id : 工作机构ID
+     * institutionsName : 工作机构名称
+     * telephone : 工作机构电话
+     */
+
+    private String id;
+    private String institutionsName;
+    private String telephone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getInstitutionsName() {
+        return institutionsName;
+    }
+
+    public void setInstitutionsName(String institutionsName) {
+        this.institutionsName = institutionsName;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
